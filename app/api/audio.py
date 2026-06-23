999de511d65b87a03c287a44487892973d4abeb3
Arnish19 <arnishmistry19@gmail.com>
2026-06-23 09:25:25 +0530

    feat: add audio transcription endpoint

--- app/api/audio.py
+++ app/api/audio.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, UploadFile, File, HTTPException
+from app.modules.transcriber import transcribe_audio
 
 from app.modules.audio_handler import (
     is_valid_audio_file,
@@ -28,4 +29,22 @@ async def upload_audio(file: UploadFile = File(...)):
         "message": "File uploaded successfully",
         "filename": file.filename,
         "path": file_path
+    }
+
+@router.post("/transcribe")
+async def transcribe_uploaded_audio(file: UploadFile = File(...)):
+
+    if not is_valid_audio_file(file.filename):
+        raise HTTPException(
+            status_code=400,
+            detail="Only wav, mp3 and m4a files are allowed"
+        )
+
+    file_path = save_audio_file(file)
+
+    transcript = transcribe_audio(file_path)
+
+    return {
+        "filename": file.filename,
+        "transcript": transcript
     }
